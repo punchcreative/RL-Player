75c116c6df65cc8162c5992a006a321b797e05f5
Erik <erik@punchcreative.nl>
2025-04-06 11:32:39 +0200

    [Erik] added and styling toplay and history

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ const URL_STREAMING = "http://kvpn:8000/stream";
 //API URL /
 const PlayerData = "playlist.json";
 
-// Visit https://api.vagalume.com.br/docs/ to get your API key
-// const API_KEY = "18fe07917957c289983464588aabddfb";
-
 let userInteracted = true;
 
 let musicaAtual = null;
@@ -26,17 +23,8 @@ window.addEventListener("load", () => {
 
   getStreamingData();
 
-  // Define interval voor vernieuwen playlist data
-  const streamingInterval = setInterval(getStreamingData, 5000);
-
-  // Ajusta a altura da capa do álbum para ser igual à sua largura
-  //   const coverArt = document.querySelector(".cover-album"); // Use querySelector para selecionar o elemento
-  //   if (coverArt) {
-  //     // Adiciona uma verificação para garantir que o elemento exista
-  //     coverArt.style.height = `${coverArt.offsetWidth}px`;
-  //   } else {
-  //     console.warn("Elemento .cover-album não encontrado.");
-  //   }
+  // Define interval te renew playlist data in millisecsonds
+  const streamingInterval = setInterval(getStreamingData, 10000);
 });
 
 // DOM control
@@ -49,7 +37,6 @@ class Page {
     this.refreshCurrentSong = function (song, artist) {
       const currentSong = document.getElementById("currentSong");
       const currentArtist = document.getElementById("currentArtist");
-      // const lyricsSong = document.getElementById("lyricsSong");
 
       if (
         song !== currentSong.textContent ||
@@ -63,7 +50,6 @@ class Page {
           // Atualizar o conteúdo após o fade-out
           currentSong.textContent = song;
           currentArtist.textContent = artist;
-          // lyricsSong.textContent = song + " - " + artist;
 
           // Esmaecer o novo conteúdo (fade-in)
           currentSong.classList.remove("fade-out");
@@ -82,49 +68,7 @@ class Page {
 
     const defaultCoverArt = "img/cover.png";
 
-    //   // Extrai o título da música e o nome do artista,
-    //   // tratando a possibilidade de 'song' e 'artist' serem objetos ou strings.
-    //   const songTitle =
-    //     typeof info.song === "object" ? info.song.title : info.song;
-    //   const songArtist =
-    //     typeof info.artist === "object" ? info.artist.title : info.artist;
-
-    //   // Define o conteúdo dos elementos HTML,
-    //   // incluindo uma verificação para evitar erros caso os valores estejam ausentes.
-    //   songName.innerHTML = songTitle || "Desconhecido";
-    //   artistName.innerHTML = songArtist || "Desconhecido";
-
-    //   try {
-    //     // Utiliza os valores extraídos para buscar a capa do álbum na API do iTunes.
-    //     const data = await getDataFromITunes(
-    //       songArtist,
-    //       songTitle,
-    //       defaultCoverArt,
-    //       defaultCoverArt
-    //     );
-    //     // Define a imagem de fundo do elemento 'coverHistoric' com a capa encontrada.
-    //     coverHistoric.style.backgroundImage =
-    //       "url(" + (data.art || defaultCoverArt) + ")";
-    //   } catch (error) {
-    //     // Captura e imprime o erro no console para ajudar na depuração.
-    //     console.log("Erro ao buscar dados da API do iTunes:");
-    //     console.error(error);
-    //     // Define a imagem de fundo como a capa padrão em caso de erro.
-    //     coverHistoric.style.backgroundImage = "url(" + defaultCoverArt + ")";
-    //   }
-
-    //   // Adiciona a classe 'animated' para a animação de slide.
-    //   historicDiv.classList.add("animated", "slideInRight");
-    //   // Remove a classe 'animated' após 2 segundos para preparar para a próxima animação.
-    //   setTimeout(
-    //     () => historicDiv.classList.remove("animated", "slideInRight"),
-    //     2000
-    //   );
-    // };
-
     this.changeVolumeIndicator = function (volume) {
-      // document.getElementById("volIndicator").textContent = volume; // Use textContent em vez de innerHTML
-
       if (typeof Storage !== "undefined") {
         localStorage.setItem("volume", volume);
       }
@@ -132,10 +76,9 @@ class Page {
 
     this.setVolume = function () {
       if (typeof Storage !== "undefined") {
-        const volumeLocalStorage = localStorage.getItem("volume") || 80; // Operador de coalescência nula (??)
+        const volumeLocalStorage = localStorage.getItem("volume") || 80;
 
         document.getElementById("volume").value = volumeLocalStorage;
-        // document.getElementById("volIndicator").textContent = volumeLocalStorage;
       }
     };
   }
@@ -161,45 +104,76 @@ async function getStreamingData() {
       if (safeCurrentSong !== musicaAtual) {
         document.title = `${safeCurrentSong} - ${safeCurrentArtist} | ${RADIO_NAME}`;
 
-        // page.refreshCover(safeCurrentSong, safeCurrentArtist);
         page.refreshCurrentSong(safeCurrentSong, safeCurrentArtist);
-        // page.refreshLyric(safeCurrentSong, safeCurrentArtist);
+
+        const toplayContainer = document.getElementById("toplaySong");
+        toplayContainer.innerHTML = "";
+
+        const toplayArray = data.Next
+          ? data.Next.map((item) => ({
+              Title: item.Title,
+              Artist: item.Artist,
+            }))
+          : data.Next;
+
+        console.log("Toplay Array:", toplayArray);
+
+        const maxToplayToDisplay = 5; // Adjust as needed
+        const limitedToplay = toplayArray.slice(
+          Math.max(0, toplayArray.length - maxToplayToDisplay)
+        );
+
+        for (let i = 0; i < limitedToplay.length; i++) {
+          const songInfo = limitedToplay[i];
+          const textSize = "text-size-" + i;
+          const article = document.createElement("article");
+          article.classList.add("col-12");
+          article.innerHTML = `
+                        <div class="music-info text-center">
+                          <p class="song ${textSize}">${
+            songInfo.Artist || "Unknown"
+          } - ${songInfo.Title || "Unknown"}</p>
+                        </div>
+                      `;
+          toplayContainer.appendChild(article);
+        }
 
         const historicContainer = document.getElementById("historicSong");
         historicContainer.innerHTML = "";
 
-        // const historyArray = data.song_history
-        //   ? data.song_history.map((item) => ({
-        //       song: item.song.title,
-        //       artist: item.song.artist,
-        //     }))
-        //   : data.history;
+        const historyArray = data.Last
+          ? data.Last.map((item) => ({
+              Title: item.Title,
+              Artist: item.Artist,
+            }))
+          : data.Last;
 
-        // const maxSongsToDisplay = 2; // Adjust as needed
-        // const limitedHistory = historyArray.slice(
-        //   Math.max(0, historyArray.length - maxSongsToDisplay)
-        // );
+        console.log("History Array:", historyArray);
 
-        // for (let i = 0; i < limitedHistory.length; i++) {
-        //   const songInfo = limitedHistory[i];
-        //   const article = document.createElement("article");
-        //   article.classList.add("col-12", "col-md-6");
-        //   article.innerHTML = `
-        //                 <div class="cover-historic" style="background-image: url('img/cover.png');"></div>
-        //                 <div class="music-info">
-        //                   <p class="song">${songInfo.song || "Desconhecido"}</p>
-        //                   <p class="artist">${
-        //                     songInfo.artist || "Desconhecido"
-        //                   }</p>
-        //                 </div>
-        //               `;
-        //   historicContainer.appendChild(article);
-        //   try {
-        //     page.refreshHistoric(songInfo, i);
-        //   } catch (error) {
-        //     console.error("Error refreshing historic song:", error);
-        //   }
-        // }
+        const maxHistoryToDisplay = 2; // Adjust as needed
+        const limitedHistory = historyArray.slice(
+          Math.max(0, historyArray.length - maxHistoryToDisplay)
+        );
+
+        for (let i = 0; i < limitedHistory.length; i++) {
+          const songInfo = limitedHistory[i];
+          const textSize = "text-size-" + i;
+          const article = document.createElement("article");
+          article.classList.add("col-12");
+          article.innerHTML = `
+                        <div class="music-info text-center">
+                          <p class="song ${textSize}">${
+            songInfo.Artist || "Unknown"
+          } - ${songInfo.Title || "Unknown"}</p>
+                        </div>
+                      `;
+          historicContainer.appendChild(article);
+          // try {
+          //   page.refreshHistoric(songInfo, i);
+          // } catch (error) {
+          //   console.error("Error refreshing historic song:", error);
+          // }
+        }
         musicaAtual = safeCurrentSong;
       }
     }
@@ -239,6 +213,7 @@ function changeImageSize(url, size) {
 
 // Variável global para armazenar as músicas
 // var audio = new Audio(URL_STREAMING);
+var audio = new Audio();
 
 // Player control
 class Player {
